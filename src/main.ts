229eac4d6027ffd2f219fb0b6a9a4d99eaf2bb65
Radmacher <git@radmacher.me>
2021-11-19 22:28:38 +0100

    fixes to store initialization

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import { createPinia } from 'pinia'
 import 'virtual:windi.css';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(createPinia()).mount('#app');
