bd19f87956e1e5908b58797c2bf9be676cdf38c8
Radmacher <git@radmacher.me>
2021-11-20 15:29:19 +0100

    Changed button label

--- src/views/form.vue
+++ src/views/form.vue
@@ -16,8 +16,7 @@
         </label>
       </form>
       <div class="flex flex-row justify-end px-5 mb-2">
-        <button type="submit" @click="submit" class="border border-dark-900 rounded-lg px-3 py-1 mt-2">Sign up</button>
-        <button type="submit" @click="clear" class="border border-dark-900 rounded-lg px-3 py-1 mt-2">Reset</button>
+        <button type="submit" @click="submit" class="border border-dark-900 rounded-lg px-3 py-1 mt-2">Submit</button>
       </div>
     </div>
   </div>
